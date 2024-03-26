57263aa44f90fc32aeb19bfee1a0395f824e25c5
shriyans Jindal <shriyansjindal@shriyanss-MacBook-Air.local>
2024-03-26 16:07:07 +0530

    landing page added

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import "./Navbar.css"
+import logo from "../assets/logo.png"
+import AccountMenu from './AccountMenu'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+    const [isloggedin,SetIsloggedin]=useState(false)
+  return (
+    <div>
+      <nav>
+      {/* Navbar */}
+        <div className="navbar">
+        {/* Navitems */}
+        <div className='nav-items'>
+            <ul>
+                <li>Home</li>
+                <li>Colors</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        {/* Logo */}
+        <div className='logo'>
+            <img src={logo} alt="" />
+        </div>
+        {/* Login box */}
+        <div className='login'>
+            {isloggedin==true ? (
+                <AccountMenu/>
+):(            <div>
+  {/* Sign in /singup buttonss */}
+  <Link to={"/login"}><button className='Signin'>Sign in</button></Link>
+    
+    <button className='Signup'>Sign up</button></div>)}
+            {/* <button className='Signin'>Sign in</button>
+            <button className='Signup'>Sign up</button> */}
+            
+        </div>
+        </div>
+
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
