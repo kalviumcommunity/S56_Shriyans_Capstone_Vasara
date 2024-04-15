76a8ec9643036fcbb0cc7baf2ac4720e6f6f5b38
shriyans Jindal <shriyansjindal@shriyanss-MacBook-Air.local>
2024-04-15 17:10:24 +0530

    changes made

--- client/src/Components/AccountMenu.jsx
+++ client/src/Components/AccountMenu.jsx
@@ -25,7 +25,7 @@ export default function AccountMenu() {
   };
   let handleLogout=()=>{
     Cookies.remove('token', { path: '' })
-    // window.location.reload()
+    window.location.reload()
   }
   return (
     <React.Fragment>
